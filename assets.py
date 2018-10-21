4719bbc267c46ab7f3a14c7ca95e1e647a593e96
Nicholas Car <nicholas.car@csiro.au>
2018-10-21 13:26:17 +1000

    Create assets.py

---
+++ assets.py
@@ -0,0 +1,11 @@
+ASSETS = [
+    {
+        'label': 'AGRIF Ontology',
+        'uris': [
+            {
+                'address': 'http://linked.data.gov.au/def/agrif',
+                'regex': '<h1>The Australian Government Records Interoperability Framework (AGRIF) ontology</h1>'
+            }
+        ]
+    }
+]
